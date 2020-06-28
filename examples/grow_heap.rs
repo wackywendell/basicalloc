2e69fe71309d8f46c67669784164753b570b9272
Wendell Smith <wackywendell@gmail.com>
2020-06-28 12:54:40 -0400

    Added grow_heap example

---
+++ examples/grow_heap.rs
@@ -0,0 +1,21 @@
+//! This is a very minimal example to show using the HeapGrower functions.
+
+#[cfg(feature = "use_libc")]
+use basic_allocator::allocators::LibcHeapGrower;
+use basic_allocator::allocators::{HeapGrower, SyscallHeapGrower};
+
+fn main() {
+    #[cfg(feature = "use_libc")]
+    {
+        // LibcHeapGrower uses libc to call mmap
+        let mut lhg = LibcHeapGrower::default();
+        let (p, sz) = unsafe { lhg.grow_heap(8) };
+        println!("Returned: ({:p}, {})", p, sz);
+    }
+
+    // SyscallHeapGrower uses inline assembly to make a direct mmap syscall.
+    // This requires nightly
+    let mut shg = SyscallHeapGrower::default();
+    let (p, sz) = unsafe { shg.grow_heap(8) };
+    println!("Returned: ({:p}, {})", p, sz);
+}
