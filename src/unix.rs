7aebff701f37c55752cf30de0361d6777a434f19
Wendell Smith <wackywendell@gmail.com>
2020-07-02 23:49:06 -0400

    Update src/unix.rs for linux constants

--- src/unix.rs
+++ src/unix.rs
@@ -1,3 +1,8 @@
+/// Some unix constants and an mmap function using assembly.
+///
+/// The constants were taken from sys/mman.h, and have been tested on osx and
+/// linux.
+
 //============================================================
 // System call code
 #[cfg(target_os = "macos")]
@@ -8,31 +13,36 @@ const SYS_MMAP: i64 = 0x09;
 //============================================================
 // Flags for protection
 
+// These seem to be the same across linux and OS X
 pub const PROT_READ: u64 = 0x01; // [MC2] pages can be read
 pub const PROT_WRITE: u64 = 0x02; // [MC2] pages can be written
-pub const _PROT_EXEC: u64 = 0x04;
+const _PROT_EXEC: u64 = 0x04;
 
 //============================================================
 // Flags contain sharing type and options.
 
 //Sharing types; choose one.
-pub const _MAP_SHARED: u64 = 0x0001; // [MF|SHM] share changes
+const _MAP_SHARED: u64 = 0x0001; // [MF|SHM] share changes
 pub const MAP_PRIVATE: u64 = 0x0002; // [MF|SHM] changes are private
 
 // Other flags
-pub const _MAP_FIXED: u64 = 0x0010; // [MF|SHM] interpret addr exactly
-pub const _MAP_RENAME: u64 = 0x0020; // Sun: rename private pages to file
-pub const _MAP_NORESERVE: u64 = 0x0040; // Sun: don't reserve needed swap area
-pub const _MAP_RESERVED: u64 = 0x0080; // previously unimplemented MAP_INHERIT
-pub const _MAP_NOEXTEND: u64 = 0x0100; // for MAP_FILE, don't change file size
-pub const _MAP_HASSEMAPHORE: u64 = 0x0200; // region may contain semaphores
-pub const _MAP_NOCACHE: u64 = 0x0400; // don't cache pages for this mapping
-pub const _MAP_JIT: u64 = 0x0800; // Allocate a region that will be used for JIT purposes
+const _MAP_FIXED: u64 = 0x0010; // [MF|SHM] interpret addr exactly
+const _MAP_RENAME: u64 = 0x0020; // Sun: rename private pages to file
+const _MAP_NORESERVE: u64 = 0x0040; // Sun: don't reserve needed swap area
+const _MAP_RESERVED: u64 = 0x0080; // previously unimplemented MAP_INHERIT
+const _MAP_NOEXTEND: u64 = 0x0100; // for MAP_FILE, don't change file size
+const _MAP_HASSEMAPHORE: u64 = 0x0200; // region may contain semaphores
+const _MAP_NOCACHE: u64 = 0x0400; // don't cache pages for this mapping
+const _MAP_JIT: u64 = 0x0800; // Allocate a region that will be used for JIT purposes
 
 // Mapping type
-pub const _MAP_FILE: u64 = 0x0000; // map from file (default)
+const _MAP_FILE: u64 = 0x0000; // map from file (default)
+#[cfg(target_os = "macos")]
 pub const MAP_ANON: u64 = 0x1000; // allocated from memory, swap space
-pub const _MAP_ANONYMOUS: u64 = MAP_ANON;
+#[cfg(target_os = "linux")]
+pub const MAP_ANON: u64 = 0x20;
+
+const _MAP_ANONYMOUS: u64 = MAP_ANON;
 
 pub unsafe fn mmap(
     addr: *mut u8,
@@ -58,5 +68,6 @@ pub unsafe fn mmap(
     in("r9d") offset,
     );
 
+    // TODO error checking!
     in_out as *mut u8
 }
